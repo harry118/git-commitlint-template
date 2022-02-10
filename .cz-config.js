6914f13b3a8c4c4dc341451c299eed7fb9a8feea
君铉 <junxuan.gxw@dtyunxi.com>
2022-02-10 16:22:54 +0800

    。。。

--- .cz-config.js
+++ .cz-config.js
@@ -12,7 +12,7 @@ module.exports = {
     { value: '⏪回滚', name: '回滚:    代码回退' }
   ],
   scopes: [
-    { name: '模块1' },
+    { name: '测试' },
     { name: '模块2' },
     { name: '模块3' },
     { name: '模块4' }
