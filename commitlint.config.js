3bd22193c075d130479b6b5c6c40ca896c56e665
君铉 <junxuan.gxw@dtyunxi.com>
2022-02-10 17:20:37 +0800

    111

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: ["cz"],
   // extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": ["feat", "fix", "docs", "style", "refactor", "test", "revert"],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "revert"],
+    ],
   },
 };
