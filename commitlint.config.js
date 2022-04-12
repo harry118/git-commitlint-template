9788fcf52a27d01ac0f23b66bea37e3826c9806b
君铉 <junxuan.gxw@dtyunxi.com>
2022-04-12 14:18:33 +0800

    ✨feat(测试1): dsfsdfsdfsf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // extends: ["cz"],
-  extends: ["@commitlint/config-conventional"],
+  extends: ["cz"],
+  // extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [
       2,
